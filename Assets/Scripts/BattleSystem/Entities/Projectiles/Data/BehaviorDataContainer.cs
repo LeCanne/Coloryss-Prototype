91a9fa84411946837f77b45acb64cdb37b2bb16c
Le Canne <94301906+LeCanne@users.noreply.github.com>
2026-01-08 17:22:21 +0100

    Fix tooltips

--- Assets/Scripts/BattleSystem/Entities/Projectiles/Data/BehaviorDataContainer.cs
+++ Assets/Scripts/BattleSystem/Entities/Projectiles/Data/BehaviorDataContainer.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class BehaviorDataContainer
 {
-    [Description("ProjectileBehavior desired for this container")]
+    [Tooltip("ProjectileBehaviors desired for this container")]
     [SerializeField] public List<ProjectileBehavior> behaviors;
-    [Description("Handles how many seconds to elapse until behavior is done")]
+    [Tooltip("Handles how many seconds to elapse until behavior is done")]
     [SerializeField] [Min (0)] public float timeToComplete;
-    [Description("Handles how many loops are necessary before  it's taken from the list \n 0: Loops Infinitely")]
+    [Tooltip("Handles how many loops are necessary before  it's taken from the list \n 0: Loops Infinitely")]
     [SerializeField] [Min (0)] public int loops;
 
 }
